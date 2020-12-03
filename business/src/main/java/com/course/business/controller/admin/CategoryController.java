a97cc876cb785bdd4542428383055e29f117f67d
taest <876239615@qq.com>
2020-12-03 19:04:18 +0800

    分类基本代码生成

---
+++ business/src/main/java/com/course/business/controller/admin/CategoryController.java
@@ -0,0 +1,75 @@
+package com.course.business.controller.admin;
+
+import com.course.server.domain.Category;
+import com.course.server.domain.Test;
+import com.course.server.dto.CategoryDto;
+import com.course.server.dto.PageDto;
+import com.course.server.dto.ResponseDto;
+import com.course.server.exception.ValidatorException;
+import com.course.server.service.CategoryService;
+import com.course.server.service.TestService;
+import com.course.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
+
+import javax.xml.bind.ValidationException;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: Taest
+ * @CreateDate: 2020/10/28$ 20:40$
+ */
+@RestController
+@RequestMapping("/admin/category")
+public class CategoryController {
+
+    private static final Logger Log = LoggerFactory.getLogger(CategoryController.class);
+
+    public static final String BUSINESS_NAME = "分类";
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 列表查询
+     */
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto){
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    /**
+     * 列表保存 id为空则添加，反正修改
+     */
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody CategoryDto categoryDto){
+
+
+         // 保存校验
+        ValidatorUtil.require(categoryDto.getParent(), "父id");
+        ValidatorUtil.require(categoryDto.getName(), "名称");
+        ValidatorUtil.length(categoryDto.getName(), "名称", 1, 50);
+
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.save(categoryDto);
+        responseDto.setContent(categoryDto);
+        return responseDto;
+    }
+
+    /**
+     * 列表删除
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto delete(@PathVariable("id") String id){
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.delete(id);
+        return responseDto;
+    }
+}
---
+++ server/src/main/java/com/course/server/service/CategoryService.java
@@ -0,0 +1,82 @@
+package com.course.server.service;
+
+import com.course.server.domain.Category;
+import com.course.server.domain.CategoryExample;
+import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
+import com.course.server.dto.CategoryDto;
+import com.course.server.dto.PageDto;
+import com.course.server.mapper.CategoryMapper;
+import com.course.server.mapper.TestMapper;
+import com.course.server.util.CopyUtil;
+import com.course.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class CategoryService {
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    /**
+     * 查询
+     */
+    public void list(PageDto pageDto){
+        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
+        CategoryExample categoryExample = new CategoryExample();
+         categoryExample.setOrderByClause("sort asc");
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+        PageInfo<Category> pageInfo = new PageInfo<>(categoryList);
+        pageDto.setTotal(pageInfo.getTotal());
+        List<CategoryDto> categoryDtoList = new ArrayList<>();
+        for (int i = 0; i < categoryList.size(); i++) {
+            Category category = categoryList.get(i);
+            CategoryDto categoryDto = new CategoryDto();
+            BeanUtils.copyProperties(category,categoryDto);
+            categoryDtoList.add(categoryDto);
+        }
+        pageDto.setList(categoryDtoList);
+    }
+
+    /**
+     * 保存  id为空则添加，反正修改
+     */
+    public void save(CategoryDto categoryDto){
+        Category category = CopyUtil.copy(categoryDto, Category.class);
+        if(StringUtils.isEmpty(categoryDto.getId())){
+            this.insert(category);
+        }else {
+            this.update(category);
+        }
+    }
+    /**
+     * 添加
+     */
+    private void insert(Category category){
+        category.setId(UuidUtil.getShortUuid());
+        categoryMapper.insert(category);
+    }
+
+    /**
+     * 修改
+     */
+    private void update(Category category){
+        categoryMapper.updateByPrimaryKey(category);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id){
+        categoryMapper.deleteByPrimaryKey(id);
+    }
+
+
+}
